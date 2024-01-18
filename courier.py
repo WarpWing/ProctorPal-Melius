8c066cced3830534313447d7149573ba915fb6ae
Ty Chermsirivatana <chermsit@dickinson.edu>
2024-01-18 17:33:03 -0500

    Update courier.py

--- courier.py
+++ courier.py
@@ -26,18 +26,20 @@ logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 # Use Voyager Lite Embeddings
 model_name = "voyage-lite-01-instruct"
 
-voyage_api_key = st_secrets["voyage_key"]
+voyage_api_key = os.environ.get("VOYAGE_API_KEY", "")
 
 embed_model = VoyageEmbedding(
     model_name=model_name, voyage_api_key=voyage_api_key
 )
 
-if "messages" not in st.session_state.keys(): 
+
+# Update Custom QA Template with Gaia Information
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Hello, my name is Courier. I'm an Generative AI Assistant designed to help Proctor Academy students. Ask me anything about the Proctor Handbook or any current Proctor Academy staff member as of 2023-2024."}
+        {"role": "assistant", "content": "Hello, my name is Courier. I'm an Generative AI Assistant designed to help Proctor Academy students. Ask me anything about the Proctor Handbook or any current Proctor Academy staff."}
     ]
 
-openai.api_key = st_secrets["openai_key"]
+openai.api_key = ""
 
 @st.cache_resource(show_spinner=False)
 def load_data():
@@ -45,12 +47,12 @@ def load_data():
         docs = SimpleDirectoryReader(input_dir="./data", recursive=True).load_data()
         qdrant_client = QdrantClient(
             url="https://02aec354-4932-4062-9e00-422eacb506fc.us-east4-0.gcp.cloud.qdrant.io",
-            api_key=st_secrets["qdrant_key"],
+            api_key="",
         )
-        qdrant_client.create_collection(collection_name="courierv2",vectors_config=VectorParams(size=1024, distance=Distance.EUCLID),)
-        service_context = ServiceContext.from_defaults(embed_model=embed_model,llm=OpenAI(model="gpt-4", max_tokens=1500, temperature=0.5, system_prompt="Keep your answers technical and based on facts and do not hallucinate in responses. In addition, make sure all responses look natural, no Answer: or Query: in the response. Courier can also respond in any language including but not limited to English,Spanish,German,Dutch,Chinese,Thai,Korean and Japanese.  Try to keep translation short to about 4-5 sentences. Always attempt to query database."))
+        qdrant_client.create_collection(collection_name="courierv52",vectors_config=VectorParams(size=1024, distance=Distance.EUCLID),)
+        service_context = ServiceContext.from_defaults(embed_model=embed_model,llm=OpenAI(model="gpt-4", max_tokens=1500, temperature=0.5, system_prompt="Keep your answers technical and based on facts and do not hallucinate in responses. In addition, make sure all responses look natural, no Answer: or Query: in the response. Always attempt to query database."))
         set_global_service_context(service_context)
-        vector_store = QdrantVectorStore(client=qdrant_client, collection_name="courierv2")
+        vector_store = QdrantVectorStore(client=qdrant_client, collection_name="courierv52")
         storage_context = StorageContext.from_defaults(vector_store=vector_store)
         index = VectorStoreIndex.from_documents(
             docs, storage_context=storage_context, service_context=service_context,
@@ -59,18 +61,20 @@ def load_data():
 
 index = load_data()
 
-if "chat_engine" not in st.session_state.keys():
+if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
     st.session_state.chat_engine = index.as_chat_engine(streaming=True,chat_mode="condense_question",max_tokens=1500,verbose=True)
 
-if prompt := st.chat_input("Your question"):
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-for message in st.session_state.messages: 
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
         st.write(message["content"])
 
+# If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
-            res_box = st.empty()  
+            res_box = st.empty()  # Placeholder for the response text
             with st.spinner("Thinking..."):
                 response = st.session_state.chat_engine.stream_chat(prompt)
                 full_response = ""
