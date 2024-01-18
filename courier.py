0bc97ab633fc53b996da8377c3cc70a415aff009
Ty Chermsirivatana <chermsit@dickinson.edu>
2024-01-18 12:19:53 -0500

    Cleaned up imported code from Gaia

--- courier.py
+++ courier.py
@@ -41,16 +41,16 @@ openai.api_key = st_secrets["openai_key"]
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text=f"Loading Gaia v{version} ..."):
+    with st.spinner(text=f"Loading Courier v{version} ..."):
         docs = SimpleDirectoryReader(input_dir="./data", recursive=True).load_data()
         qdrant_client = QdrantClient(
             url="https://02aec354-4932-4062-9e00-422eacb506fc.us-east4-0.gcp.cloud.qdrant.io",
             api_key=st_secrets["qdrant_key"],
         )
         qdrant_client.create_collection(collection_name="courierv2",vectors_config=VectorParams(size=1024, distance=Distance.EUCLID),)
-        service_context = ServiceContext.from_defaults(embed_model=embed_model,llm=OpenAI(model="gpt-4", max_tokens=1500, temperature=0.5, system_prompt="Keep your answers technical and based on facts and do not hallucinate in responses. In addition, make sure all responses look natural, no Answer: or Query: in the response.Gaia can also respond in any language including but not limited to English,Spanish,German,Dutch,Chinese,Thai,Korean and Japanese.  Try to keep translation short to about 4-5 sentences. Always attempt to query database."))
+        service_context = ServiceContext.from_defaults(embed_model=embed_model,llm=OpenAI(model="gpt-4", max_tokens=1500, temperature=0.5, system_prompt="Keep your answers technical and based on facts and do not hallucinate in responses. In addition, make sure all responses look natural, no Answer: or Query: in the response. Courier can also respond in any language including but not limited to English,Spanish,German,Dutch,Chinese,Thai,Korean and Japanese.  Try to keep translation short to about 4-5 sentences. Always attempt to query database."))
         set_global_service_context(service_context)
-        vector_store = QdrantVectorStore(client=qdrant_client, collection_name="gaiafinal317")
+        vector_store = QdrantVectorStore(client=qdrant_client, collection_name="courierv2")
         storage_context = StorageContext.from_defaults(vector_store=vector_store)
         index = VectorStoreIndex.from_documents(
             docs, storage_context=storage_context, service_context=service_context,
